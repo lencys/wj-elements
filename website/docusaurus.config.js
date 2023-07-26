b825c6a8c33cc4d34778522b1271dedf5288407e
londrejcek <lukas.ondrejcek@interway.sk>
2023-07-26 12:36:01 +0200

    Uprava grafiky

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
     ({
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Meta Project',
+        title: '',
         logo: {
           alt: 'My Meta Project Logo',
           src: 'img/logo.svg',
@@ -75,7 +75,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Dokumentácia',
           },
           {to: 'blog', label: 'Blog', position: 'left'},
           // Please keep GitHub link to the right for consistency.
