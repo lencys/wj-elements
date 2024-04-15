946e3095d423f4f274e351459ebed0f6cd925ef0
Oliver Semko <oliver.semko@interway.sk>
2024-04-15 13:46:28 +0200

    animation

--- packages/wje-reorder/reorder.element.js
+++ packages/wje-reorder/reorder.element.js
@@ -1,69 +1,65 @@
-import { default as WJElement } from "../wje-element/element.js";
+import WJElement from "../wje-element/element.js";
 import styles from "./styles/styles.css?inline";
 
 /**
- * The `Reorder` class is a subclass of the `WJElement` class. It provides functionality for reordering items using drag and drop.
+ * The `Reorder` class is a subclass of the `WJElement` class. It provides functionality for reordering items in a list by dragging and dropping.
  */
 export default class Reorder extends WJElement {
   /**
-   * Creates a new instance of the `Reorder` class.
+   * Constructs a new instance of the `Reorder` class.
    */
   constructor() {
     super();
     this.className = "Reorder";
-    this.draggableItem = null;
+    this.dragEl = null;
     this.items = [];
+    this.prevRect = null;
+    this.originalIndex = null;
   }
 
   /**
-   * Returns the CSS styles for the `Reorder` component.
-   * @returns {string} - The CSS styles.
+   * Gets the CSS style sheet for the `Reorder` class.
+   * @returns {string} The CSS style sheet.
    */
   static get cssStyleSheet() {
     return styles;
   }
 
   /**
-   * Sets up the attributes of the `Reorder` component.
+   * Sets up the attributes for the `Reorder` class.
    */
   setupAttributes() {
     this.isShadowRoot = "open";
   }
 
   /**
-   * Overrides the `draw` method of the `WJElement` class to create a container with draggable items.
-   * @param {CanvasRenderingContext2D} context - The rendering context.
-   * @param {Object} store - The store object.
-   * @param {Object} params - The parameters object.
-   * @returns {DocumentFragment} - The document fragment containing the container.
+   * Creates and returns a document fragment containing the container element and slot for items.
+   * @param {any} context - The context.
+   * @param {any} store - The store.
+   * @param {any} params - The parameters.
+   * @returns {DocumentFragment} The document fragment.
    */
   draw(context, store, params) {
     const fragment = document.createDocumentFragment();
-
     const container = document.createElement("div");
     container.classList.add("container");
     container.setAttribute("part", "native");
-
     const slot = document.createElement("slot");
-
     container.appendChild(slot);
-
     fragment.appendChild(container);
-
     this.container = container;
-
     return fragment;
   }
 
   /**
-   * Overrides the `afterDraw` method of the `WJElement` class to handle the drag and drop events.
+   * Adds drag and drop functionality to the items after they have been rendered.
    */
   afterDraw() {
     const slots = this.shadowRoot.querySelectorAll("slot");
     slots.forEach((slot) => {
       slot.classList.add("item");
       slot.assignedElements().forEach((e) => {
-        e.classList.add("item");    
+        e.classList.add("item");
         e.draggable = true;
         e.addEventListener("dragstart", this.onDragStart.bind(this), false);
         e.addEventListener("touchstart", this.onDragStart.bind(this), false);
@@ -78,8 +74,8 @@ export default class Reorder extends WJElement {
   }
 
   /**
-   * Handles the drag start event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drag start event and sets the necessary data for the drag operation.
+   * @param {DragEvent} e - The drag start event.
    */
   onDragStart(e) {
     this.dragEl = e.currentTarget;
@@ -87,12 +83,12 @@ export default class Reorder extends WJElement {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text/html", `${this.dragEl.innerHTML}`);
     this.dragEl.style.opacity = "0.5";
-    this.originalIndex = Array.from(this.dragEl.parentNode.children).indexOf(this.dragEl);
+    this.originalIndex = [...this.dragEl.parentNode.children].indexOf(this.dragEl);
   }
 
   /**
-   * Handles the drag over event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drag over event and reorders the items based on the drag position.
+   * @param {DragEvent} e - The drag over event.
    */
   onDragOver(e) {
     e.preventDefault();
@@ -101,98 +97,97 @@ export default class Reorder extends WJElement {
       const parent = droppedElement.parentNode;
       const dragIndex = Array.from(parent.children).indexOf(this.dragEl);
       const dropIndex = Array.from(parent.children).indexOf(droppedElement);
-
       if (dragIndex < dropIndex) {
         parent.insertBefore(this.dragEl, droppedElement.nextSibling);
       } else {
         parent.insertBefore(this.dragEl, droppedElement);
       }
-
+      const items = parent.children;
+      Array.from(items).forEach((item) => {
+        if (item !== this.dragEl) {
+          const margin = droppedElement === item ? "10px" : "";
+          item.style.marginBottom = margin;
+          item.style.marginTop = margin;
+        }
+      });
       this.dragEl.classList.remove("drag--moving");
       this.dragEl.style.transform = "";
       document.querySelectorAll(".item").forEach((item) => {
-        item.style.marginTop = "";
-        item.style.marginBottom = "";
         item.classList.remove("drag--hover");
       });
     }
   }
 
   /**
-   * Handles the drag enter event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drag enter event and adds a visual indication of the drop target.
+   * @param {DragEvent} e - The drag enter event.
    */
   onDragEnter(e) {
     e.currentTarget.classList.add("drag--hover");
   }
 
   /**
-   * Handles the drag leave event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drag leave event and removes the visual indication of the drop target.
+   * @param {DragEvent} e - The drag leave event.
    */
   onDragLeave(e) {
     e.currentTarget.classList.remove("drag--hover");
   }
 
   /**
-   * Handles the drop event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drop event and reorders the items based on the drop position.
+   * @param {DragEvent} e - The drop event.
    */
   onDrop(e) {
     const droppedElement = e.currentTarget;
-
     if (this.dragEl !== droppedElement) {
       e.preventDefault();
-
       const parent = droppedElement.parentNode;
       const dragIndex = Array.from(parent.children).indexOf(this.dragEl);
       const dropIndex = Array.from(parent.children).indexOf(droppedElement);
-
       if (dragIndex < dropIndex) {
         parent.insertBefore(this.dragEl, droppedElement.nextSibling);
       } else {
         parent.insertBefore(this.dragEl, droppedElement);
       }
-
-      this.dragEl.classList.remove("drag--moving");
-      this.dragEl.style.transform = "";
-      document.querySelectorAll(".item").forEach((item) => {
-        item.style.marginTop = "";
-        item.style.marginBottom = "";
-        item.classList.remove("drag--hover");
-      });
     }
   }
 
   /**
-   * Handles the drag end event.
-   * @param {DragEvent} e - The drag event.
+   * Handles the drag end event and dispatches a custom event with the new order of the items.
+   * @param {DragEvent} e - The drag end event.
    */
   onDragEnd(e) {
     this.dragEl.classList.remove("drag--moving");
     this.dragEl.style.transform = "";
     document.querySelectorAll(".item").forEach((item) => {
-      item.classList.remove("drag--hover");
+      item.style.marginTop = "";
+      item.style.marginBottom = "";
     });
-
-    const newIndex = Array.from(this.dragEl.parentNode.children).indexOf(this.dragEl);
-    const newOrder = Array.from(this.dragEl.parentNode.children).map(el => el.textContent || el.innerText);
-
+    const newIndex = Array.from(this.dragEl.parentNode.children).indexOf(
+      this.dragEl
+    );
+    const newOrder = Array.from(this.dragEl.parentNode.children).map(
+      (el) => el.textContent || el.innerText
+    );
     this.dispatchChange(this.originalIndex, newIndex, newOrder);
-
     this.dragEl.style.opacity = "1";
   }
 
+  /**
+   * Dispatches a custom event with the original and new indexes of the dragged item and the new order of the items.
+   * @param {number} from - The original index of the dragged item.
+   * @param {number} to - The new index of the dragged item.
+   * @param {string[]} order - The new order of the items.
+   */
   dispatchChange(from, to, order) {
     console.log("FROM:", from);
     console.log("TO:", to);
     console.log("ORDER:", order);
     this.dispatchEvent(
-        new CustomEvent("wje-reorder:change", {
-            from: from,
-            to: to,
-            order: order
-        })
+      new CustomEvent("wje-reorder:change", {
+        detail: { from, to, order },
+      })
     );
-  }  
+  }
 }
