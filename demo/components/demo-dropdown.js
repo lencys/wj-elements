a979217e9a698de0798244f31a7fb31ae3afa3e7
lencys <lukas.ondrejcek@gmail.com>
2024-04-18 16:47:44 +0200

    [#56449 - WJ Elements - upravy] - uloha #18 =>

--- demo/components/demo-dropdown.js
+++ demo/components/demo-dropdown.js
@@ -96,6 +96,34 @@ template.innerHTML = `
           </style>
       </div>
     </div>
+    
+    <!-- INNER AVATAR -->
+
+    <h3>Inner avatar</h3>
+    <div class="playground">
+      <div class="content">
+        <wje-dropdown id="custom-dropdown" placement="right-start" offset="5">
+          <wje-avatar label="Petr Rahman" initials slot="trigger"></wje-avatar>
+          <wje-menu class="custom-menu" active>
+            <wje-avatar>
+              <wje-img src="/assets/img/avatar.svg"></wje-img>
+            </wje-avatar>
+            <h5 style="margin: 0;">Petr Rahman</h5>
+            <p class="hint-text">Frontend Developer</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+            <wje-divider></wje-divider>
+            <wje-menu-item>Link A</wje-menu-item>
+            <wje-menu-item>Link B</wje-menu-item>
+          </wje-menu>
+        </wje-dropdown>
+        <style>
+          .custom-menu {
+            width: 200px;
+            padding: 1rem 1rem 1rem;
+          }
+        </style>
+      </div>
+    </div>
   </div>`;
 
 export default class DemoDropdown extends WJElement {
