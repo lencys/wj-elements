ecf9f3380074dbcabda583e850bc6821e86c09e6
RHR <rastislav.hrompa@interway.sk>
2024-05-31 15:53:35 +0200

    mirage start before elements are loaded

--- demo/assets/js/demo.js
+++ demo/assets/js/demo.js
@@ -1,21 +1,21 @@
 document.querySelector(".dark-light-mode").addEventListener("wje-button:click", () => {
-  document.body.classList.toggle("wje-theme-dark");
-  saveModePreference();
+    document.body.classList.toggle("wje-theme-dark");
+    saveModePreference();
 });
 loadModePreference();
 
 function saveModePreference() {
-  var body = document.body;
-  var isDarkMode = body.classList.contains("wje-theme-dark");
-  localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+    var body = document.body;
+    var isDarkMode = body.classList.contains("wje-theme-dark");
+    localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
 }
 
 function loadModePreference() {
-  const body = document.body;
-  const darkMode = localStorage.getItem("darkMode");
-  if (darkMode === "enabled") {
-    body.classList.add("wje-theme-dark");
-  } else {
-    body.classList.remove("wje-theme-dark");
-  }
+    const body = document.body;
+    const darkMode = localStorage.getItem("darkMode");
+    if (darkMode === "enabled") {
+        body.classList.add("wje-theme-dark");
+    } else {
+        body.classList.remove("wje-theme-dark");
+    }
 }
