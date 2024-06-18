3247e91c7071a550a1e0d7fe7f42c4fb1d45d187
lencys <lukas.ondrejcek@gmail.com>
2024-06-18 12:21:16 +0200

    Uprava timeline a timeline item

--- packages/wje-timeline-item/timeline-item.element.js
+++ packages/wje-timeline-item/timeline-item.element.js
@@ -1,86 +1,95 @@
-import { default as WJElement } from "../wje-element/element.js";
+import { default as WJElement, WjElementUtils } from "../wje-element/element.js";
 import styles from "./styles/styles.css?inline";
 
+/**
+ * The TimelineItem component.
+ * @summary This element represents a timeline item.
+ * @documentation https://elements.webjet.sk/components/timeline-item
+ * @status stable
+ *
+ * @extends {WJElement}
+ *
+ * @part native - The native part of the timeline item.
+ *
+ * @slot - Slot for the content of the timeline item.
+ * @slot status - Slot for the status of the timeline item.
+ *
+ * @tag wje-timeline-item
+ */
 export default class TimelineItem extends WJElement {
     constructor() {
         super();
     }
 
+    /**
+     * Returns the class name of the tab.
+     *
+     * @returns {string} The class name of the tab.
+     */
     className = "TimelineItem";
 
+    /**
+     * Returns the CSS styles for the component.
+     *
+     * @static
+     * @returns {CSSStyleSheet}
+     */
     static get cssStyleSheet() {
         return styles;
     }
 
+    /**
+     * Sets up the attributes for the component.
+     */
     setupAttributes() {
         this.isShadowRoot = "open";
     }
 
-    createStatusElement(type) {
-        const status = document.createElement('wje-status');
-        const icon = document.createElement('wje-icon');
-        status.setAttribute('slot', 'icon');
-        status.setAttribute('size', 'medium');
-        icon.setAttribute('size', 'medium');
-        switch (type) {
-            case 'active':
-                icon.setAttribute('name', 'check');
-                status.setAttribute('color', 'success');
-                break;
-            case 'default':
-            default:
-                status.setAttribute('color', 'contrast');
-                break;
-        }
-
-        status.appendChild(icon);
-
-        return status;
-    }
-
-    wrapInCard(slot) {
-        const card = document.createElement("wje-card");
-        const cardHeader = document.createElement("wje-card-header");
-
-        cardHeader.appendChild(slot);
-        card.appendChild(cardHeader);
-
-        return card;
-    }
-
+    /**
+     * Draws the component.
+     *
+     * @param {Object} context - The context for drawing.
+     * @param {Object} store - The store for drawing.
+     * @param {Object} params - The parameters for drawing.
+     * @returns {DocumentFragment}
+     */
     draw(context, store, params) {
         let fragment = document.createDocumentFragment();
 
-        const native = document.createElement('div');
+        let native = document.createElement('div');
         native.setAttribute("part", "native");
         native.classList.add("native-timeline-item");
 
-        const statusElement = this.hasAttribute('active')
-            ? this.createStatusElement('active')
-            : this.createStatusElement('default');
-
-        const contentContainer = document.createElement('div');
+        let contentContainer = document.createElement('div');
         contentContainer.classList.add('content-container');
 
-        const relativeTime = document.createElement('wje-relative-time');
+        let relativeTime = document.createElement('wje-relative-time');
         relativeTime.setAttribute('date', this.getAttribute('datetime') || '');
 
-        const event = document.createElement('h3');
+        let event = document.createElement('h3');
         event.classList.add('event');
         event.textContent = this.getAttribute('event') || '';
 
-        const card = document.createElement("wje-card");
+        // additional text content
+        let slot = document.createElement('slot');
 
-        const slot = document.createElement('slot');
+        // status slot
+        let slotStatus = document.createElement('wje-icon')
+        slotStatus.setAttribute('name', 'circle-dot');
+        slotStatus.setAttribute('filled', '');
+
+        // if status slot is present
+        if(this.querySelector('[slot="status"]')) {
+            slotStatus = document.createElement('slot');
+            slotStatus.setAttribute('name', 'status');
+        }
 
         contentContainer.appendChild(relativeTime);
         contentContainer.appendChild(event);
-        if(this.innerHTML) {
-            card.appendChild(slot);
-            contentContainer.appendChild(card);
-        }
 
-        native.appendChild(statusElement);
+        contentContainer.appendChild(slot);
+
+        native.appendChild(slotStatus);
         native.appendChild(contentContainer);
 
         fragment.appendChild(native);
--- packages/wje-timeline/timeline.element.js
+++ packages/wje-timeline/timeline.element.js
@@ -1,35 +1,73 @@
 import { default as WJElement } from "../wje-element/element.js";
 import styles from "./styles/styles.css?inline";
 
+/**
+ * Timeline component.
+ * @summary This element represents a timeline.
+ * @documentation https://elements.webjet.sk/components/timeline
+ * @status stable
+ *
+ * @extends {WJElement}
+ *
+ * @part native - The native part of the rating component.
+ *
+ * @slot - Slot for the timeline items.
+ *
+ * @tag wje-timeline
+ */
 export default class Timeline extends WJElement {
     constructor() {
         super();
     }
 
+    /**
+     * Returns the class name of the tab.
+     *
+     * @returns {string} The class name of the tab.
+     */
     className = "Timeline";
 
+    /**
+     * Returns the CSS styles for the component.
+     *
+     * @static
+     * @returns {CSSStyleSheet}
+     */
     static get cssStyleSheet() {
         return styles;
     }
 
+    /**
+     * Sets up the attributes for the component.
+     */
     setupAttributes() {
         this.isShadowRoot = "open";
     }
 
+    /**
+     * Draws the component.
+     *
+     * @param {Object} context - The context for drawing.
+     * @param {Object} store - The store for drawing.
+     * @param {Object} params - The parameters for drawing.
+     * @returns {DocumentFragment}
+     */
     draw(context, store, params) {
         let fragment = document.createDocumentFragment();
 
-        const timelineContainer = document.createElement('div');
-        timelineContainer.classList.add('timeline-container');
+        const native = document.createElement('div');
+        native.setAttribute('part', 'native');
+        native.classList.add('native-timeline');
 
         const verticalLine = document.createElement('div');
         verticalLine.classList.add('vertical-line');
 
         const slot = document.createElement('slot');
-        timelineContainer.appendChild(verticalLine);
-        timelineContainer.appendChild(slot);
 
-        fragment.appendChild(timelineContainer);
+        native.appendChild(verticalLine);
+        native.appendChild(slot);
+
+        fragment.appendChild(native);
 
         return fragment;
     }
