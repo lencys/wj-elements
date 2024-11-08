eb9a69926c243bade52ad2449ede733dfafb38f8
lencys <lukas.ondrejcek@gmail.com>
2024-11-08 14:04:38 +0100

    eslint - upravy 5

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 import globals from "globals";
-
 
 export default [
   {
