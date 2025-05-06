419c31831b37a3e58f007c293b156935a6a2613d
RHR <hrompa.rastislav@gmail.com>
2025-05-06 11:03:25 +0200

    refactor: improve Checkbox class with internal value management and event handling

--- packages/wje-checkbox/checkbox.element.js
+++ packages/wje-checkbox/checkbox.element.js
@@ -18,318 +18,329 @@ import styles from './styles/styles.css?inline';
  * //@fires wje-checkbox:change - Dispatched when the checkbox state changes;
  */
 export default class Checkbox extends WJElement {
-    /**
-     * Checkbox constructor method.
-     */
-    constructor() {
-        super();
-        this.internals = this.attachInternals();
-    }
+	#internalValue = false;
+	/**
+	 * Checkbox constructor method.
+	 */
+	constructor() {
+		super();
+		this.internals = this.attachInternals();
+	}
 
-    /**
-     * Setter for the value attribute.
-     * @param {string} value The value to set.
-     */
-    set value(value) {
-        this.internals.setFormValue(value);
+	/**
+	 * Setter for the value attribute.
+	 * @param {string} value The value to set.
+	 */
+	set value(value) {
+		this.internals.setFormValue(value);
+		this.#internalValue = value;
 
-        if (this.input) this.input.value = value;
-    }
+		if (this.input) {
+			this.input.value = value;
+			this.input.checked = value;
+		}
+	}
 
-    /**
-     * Getter for the value attribute.
-     * @returns {string} The value of the attribute.
-     */
-    get value() {
-        return this.input?.value || '';
-    }
+	/**
+	 * Getter for the value attribute.
+	 * @returns {string} The value of the attribute.
+	 */
+	get value() {
+		return this.#internalValue;
+	}
 
-    /**
-     * Getter for the customErrorDisplay attribute.
-     * @returns {boolean} Whether the attribute is present.
-     */
-    get customErrorDisplay() {
-        return this.hasAttribute('custom-error-display');
-    }
+	/**
+	 * Getter for the customErrorDisplay attribute.
+	 * @returns {boolean} Whether the attribute is present.
+	 */
+	get customErrorDisplay() {
+		return this.hasAttribute('custom-error-display');
+	}
 
-    /**
-     * Getter for the validateOnChange attribute.
-     * @returns {boolean} Whether the attribute is present.
-     */
-    get validateOnChange() {
-        return this.hasAttribute('validate-on-change');
-    }
+	/**
+	 * Getter for the validateOnChange attribute.
+	 * @returns {boolean} Whether the attribute is present.
+	 */
+	get validateOnChange() {
+		return this.hasAttribute('validate-on-change');
+	}
 
-    /**
-     * Setter for the invalid attribute.
-     * @param {boolean} isInvalid Whether the input is invalid.
-     */
-    set invalid(isInvalid) {
-        if (isInvalid) this.setAttribute('invalid', '');
-        else this.removeAttribute('invalid');
-    }
+	/**
+	 * Setter for the invalid attribute.
+	 * @param {boolean} isInvalid Whether the input is invalid.
+	 */
+	set invalid(isInvalid) {
+		if (isInvalid) this.setAttribute('invalid', '');
+		else this.removeAttribute('invalid');
+	}
 
-    /**
-     * Getter for the invalid attribute.
-     * @returns {boolean} Whether the attribute is present.
-     */
-    get invalid() {
-        return this.hasAttribute('invalid');
-    }
+	/**
+	 * Getter for the invalid attribute.
+	 * @returns {boolean} Whether the attribute is present.
+	 */
+	get invalid() {
+		return this.hasAttribute('invalid');
+	}
 
-    /**
-     * Getter for the form attribute.
-     * @returns {HTMLFormElement} The form the input is associated with.
-     */
-    get form() {
-        return this.internals.form;
-    }
+	/**
+	 * Getter for the form attribute.
+	 * @returns {HTMLFormElement} The form the input is associated with.
+	 */
+	get form() {
+		return this.internals.form;
+	}
 
-    /**
-     * Getter for the name attribute.
-     * @returns {string} The name of the input.
-     */
-    get name() {
-        return this.getAttribute('name');
-    }
+	/**
+	 * Getter for the name attribute.
+	 * @returns {string} The name of the input.
+	 */
+	get name() {
+		return this.getAttribute('name');
+	}
 
-    /**
-     * Getter for the type attribute.
-     * @returns {string} The type of the input.
-     */
-    get type() {
-        return this.localName;
-    }
+	/**
+	 * Getter for the type attribute.
+	 * @returns {string} The type of the input.
+	 */
+	get type() {
+		return this.localName;
+	}
 
-    /**
-     * Getter for the validity attribute.
-     * @returns {ValidityState} The validity state of the input.
-     */
-    get validity() {
-        return this.internals.validity;
-    }
+	/**
+	 * Getter for the validity attribute.
+	 * @returns {ValidityState} The validity state of the input.
+	 */
+	get validity() {
+		return this.internals.validity;
+	}
 
-    /**
-     * Getter for the validationMessage attribute.
-     * @returns {string} The validation message of the input.
-     */
-    get validationMessage() {
-        return this.internals.validationMessage;
-    }
+	/**
+	 * Getter for the validationMessage attribute.
+	 * @returns {string} The validation message of the input.
+	 */
+	get validationMessage() {
+		return this.internals.validationMessage;
+	}
 
-    /**
-     * Getter for the willValidate attribute.
-     * @returns {boolean} Whether the input will be validated.
-     */
-    get willValidate() {
-        return this.internals.willValidate;
-    }
+	/**
+	 * Getter for the willValidate attribute.
+	 * @returns {boolean} Whether the input will be validated.
+	 */
+	get willValidate() {
+		return this.internals.willValidate;
+	}
 
-    /**
-     * Getter for the defaultValue attribute.
-     * This method retrieves the 'value' attribute of the custom input element.
-     * The 'value' attribute represents the default value of the input element.
-     * If the 'value' attribute is not set, it returns an empty string.
-     * @returns {string} The default value of the input element.
-     */
-    get defaultValue() {
-        return this.getAttribute('value') ?? '';
-    }
+	/**
+	 * Getter for the defaultValue attribute.
+	 * This method retrieves the 'value' attribute of the custom input element.
+	 * The 'value' attribute represents the default value of the input element.
+	 * If the 'value' attribute is not set, it returns an empty string.
+	 * @returns {string} The default value of the input element.
+	 */
+	get defaultValue() {
+		return this.getAttribute('value') ?? '';
+	}
 
-    /**
-     * Setter for the defaultValue attribute.
-     * This method sets the 'value' attribute of the custom input element to the provided value.
-     * The 'value' attribute represents the default value of the input element.
-     * @param {string} value The value to set as the default value.
-     */
-    set defaultValue(value) {
-        this.setAttribute('value', value);
-    }
+	/**
+	 * Setter for the defaultValue attribute.
+	 * This method sets the 'value' attribute of the custom input element to the provided value.
+	 * The 'value' attribute represents the default value of the input element.
+	 * @param {string} value The value to set as the default value.
+	 */
+	set defaultValue(value) {
+		this.setAttribute('value', value);
+	}
 
-    /**
-     * Set checked attribute.
-     * @param {boolean} value true if the toggle is checked, false otherwise
-     */
-    set disabled(value) {
-        if (value) this.setAttribute('disabled', '');
-        else this.removeAttribute('disabled');
-    }
+	/**
+	 * Set checked attribute.
+	 * @param {boolean} value true if the toggle is checked, false otherwise
+	 */
+	set disabled(value) {
+		if (value) this.setAttribute('disabled', '');
+		else this.removeAttribute('disabled');
+	}
 
-    /**
-     * Get disabled attribute value.
-     * @returns {boolean} true if the toggle is disabled, false otherwise
-     */
-    get disabled() {
-        return this.hasAttribute('disabled');
-    }
+	/**
+	 * Get disabled attribute value.
+	 * @returns {boolean} true if the toggle is disabled, false otherwise
+	 */
+	get disabled() {
+		return this.hasAttribute('disabled');
+	}
 
-    /**
-     * Set checked attribute.
-     * @param {boolean} value true if the toggle is checked, false otherwise
-     */
-    set checked(value) {
-        if (value) this.setAttribute('checked', '');
-        else this.removeAttribute('checked');
-    }
+	/**
+	 * Set checked attribute.
+	 * @param {boolean} value true if the toggle is checked, false otherwise
+	 */
+	set checked(value) {
+		if (value) {
+			this.setAttribute('checked', '');
+			this.value = true;
+		}
+		else {
+			this.removeAttribute('checked');
+			this.value = false;
+		}
+	}
 
-    /**
-     * Get checked attribute.
-     * @returns {boolean} true if the toggle is checked, false otherwise
-     */
-    get checked() {
-        return this.hasAttribute('checked');
-    }
+	/**
+	 * Get checked attribute.
+	 * @returns {boolean} true if the toggle is checked, false otherwise
+	 */
+	get checked() {
+		return this.hasAttribute('checked');
+	}
 
-    /**
-     * The class name for the Checkbox.
-     */
-    className = 'Checkbox';
+	/**
+	 * The class name for the Checkbox.
+	 */
+	className = 'Checkbox';
 
-    /**
-     * Getter for the CSS stylesheet.
-     * @returns {string} The CSS stylesheet.
-     */
-    static get cssStyleSheet() {
-        return styles;
-    }
+	/**
+	 * Getter for the CSS stylesheet.
+	 * @returns {string} The CSS stylesheet.
+	 */
+	static get cssStyleSheet() {
+		return styles;
+	}
 
-    static get observedAttributes() {
-        return ['checked', 'disabled', 'value', 'indeterminate'];
-    }
+	static get observedAttributes() {
+		return ['checked', 'disabled', 'value', 'indeterminate'];
+	}
 
-    /**
-     * Whether the input is associated with a form.
-     * @type {boolean}
-     */
-    static formAssociated = true;
+	/**
+	 * Whether the input is associated with a form.
+	 * @type {boolean}
+	 */
+	static formAssociated = true;
 
-    /**
-     * Sets up the attributes for the checkbox.
-     */
-    setupAttributes() {
-        this.isShadowRoot = 'open';
-    }
+	/**
+	 * Sets up the attributes for the checkbox.
+	 */
+	setupAttributes() {
+		this.isShadowRoot = 'open';
+	}
 
-    /**
-     * Draws the checkbox element.
-     * @returns {DocumentFragment} The created fragment.
-     */
-    draw() {
-        let fragment = document.createDocumentFragment();
+	/**
+	 * Draws the checkbox element.
+	 * @returns {DocumentFragment} The created fragment.
+	 */
+	draw() {
+		let fragment = document.createDocumentFragment();
 
-        let native = document.createElement('div');
-        native.setAttribute('part', 'native');
-        native.classList.add('native-checkbox');
+		let native = document.createElement('div');
+		native.setAttribute('part', 'native');
+		native.classList.add('native-checkbox');
 
-        let input = document.createElement('input');
-        input.type = 'checkbox';
-        input.id = 'checkbox';
-        input.name = this.name || 'checkbox';
-        input.checked = this.hasAttribute('checked');
-        input.disabled = this.hasAttribute('disabled');
-        input.indeterminate = this.hasAttribute('indeterminate');
+		let input = document.createElement('input');
+		input.type = 'checkbox';
+		input.id = 'checkbox';
+		input.name = this.name || 'checkbox';
+		input.checked = this.hasAttribute('checked');
+		input.disabled = this.hasAttribute('disabled');
+		input.indeterminate = this.hasAttribute('indeterminate');
 
-        let label = document.createElement('label');
-        label.htmlFor = 'checkbox';
-        label.innerHTML = '<slot></slot>';
+		let label = document.createElement('label');
+		label.htmlFor = 'checkbox';
+		label.innerHTML = '<slot></slot>';
 
-        native.appendChild(input);
-        native.appendChild(label);
+		native.appendChild(input);
+		native.appendChild(label);
 
-        this.input = input;
+		this.input = input;
 
-        fragment.appendChild(native);
+		fragment.appendChild(native);
 
-        return fragment;
-    }
+		return fragment;
+	}
 
-    /**
-     * Adds an event listener after drawing the checkbox.
-     */
-    afterDraw() {
-        if (!this.disabled) {
-            this.input.addEventListener('input', (e) => {
-                this.value = e.target.checked;
-                this.checked = e.target.checked;
-                event.dispatchCustomEvent(this, 'wje-toggle:input');
-            });
+	/**
+	 * Adds an event listener after drawing the checkbox.
+	 */
+	afterDraw() {
+		if (!this.disabled) {
+			this.input.addEventListener('input', (e) => {
+				this.value = e.target.checked;
+				this.checked = e.target.checked;
+				event.dispatchCustomEvent(this, 'wje-toggle:input');
+			});
 
-            this.input.addEventListener('change', (e) => {
-                this.value = e.target.checked;
-                this.checked = e.target.checked;
-                event.dispatchCustomEvent(this, 'wje-toggle:change');
-            });
-        }
-    }
+			this.input.addEventListener('change', (e) => {
+				this.value = e.target.checked;
+				this.checked = e.target.checked;
+				event.dispatchCustomEvent(this, 'wje-toggle:change');
+			});
+		}
+	}
 
-    /**
-     * Removes the event listener when the checkbox is disconnected.
-     */
-    beforeDisconnect() {
-        event.removeElement(this.input);
-    }
+	/**
+	 * Removes the event listener when the checkbox is disconnected.
+	 */
+	beforeDisconnect() {
+		event.removeElement(this.input);
+	}
 
-    /**
-     * @summary Callback function that is called when the custom element is associated with a form.
-     * This function adds an event listener to the form's submit event, which validates the input and propagates the validation.
-     * @param {HTMLFormElement} form The form the custom element is associated with.
-     */
-    formAssociatedCallback(form) {
-        if (form) {
-            this.internals.setFormValue(this.value);
-        }
-    }
+	/**
+	 * @summary Callback function that is called when the custom element is associated with a form.
+	 * This function adds an event listener to the form's submit event, which validates the input and propagates the validation.
+	 * @param {HTMLFormElement} form The form the custom element is associated with.
+	 */
+	formAssociatedCallback(form) {
+		if (form) {
+			this.internals.setFormValue(this.value);
+		}
+	}
 
-    /**
-     * The formResetCallback method is a built-in lifecycle callback that gets called when a form gets reset.
-     * This method is responsible for resetting the value of the custom input element to its default value.
-     * It also resets the form value and validity state in the form internals.
-     * @function
-     */
-    formResetCallback() {
-        // Set the value of the custom input element to its default value
-        this.value = this.defaultValue;
-        // Reset the form value in the form internals to the default value
-        this.internals.setFormValue(this.defaultValue);
-        // Reset the validity state in the form internals
-        this.internals.setValidity({});
-    }
+	/**
+	 * The formResetCallback method is a built-in lifecycle callback that gets called when a form gets reset.
+	 * This method is responsible for resetting the value of the custom input element to its default value.
+	 * It also resets the form value and validity state in the form internals.
+	 * @function
+	 */
+	formResetCallback() {
+		// Set the value of the custom input element to its default value
+		this.value = this.defaultValue;
+		// Reset the form value in the form internals to the default value
+		this.internals.setFormValue(this.defaultValue);
+		// Reset the validity state in the form internals
+		this.internals.setValidity({});
+	}
 
-    /**
-     * The formStateRestoreCallback method is a built-in lifecycle callback that gets called when the state of a form-associated custom element is restored.
-     * This method is responsible for restoring the value of the custom input element to its saved state.
-     * It also restores the form value and validity state in the form internals to their saved states.
-     * @param {object} state The saved state of the custom input element.
-     * @function
-     */
-    formStateRestoreCallback(state) {
-        // Set the value of the custom input element to its saved value
-        this.value = state.value;
-        // Restore the form value in the form internals to the saved value
-        this.internals.setFormValue(state.value);
-        // Restore the validity state in the form internals to the saved state
-        this.internals.setValidity({});
-    }
+	/**
+	 * The formStateRestoreCallback method is a built-in lifecycle callback that gets called when the state of a form-associated custom element is restored.
+	 * This method is responsible for restoring the value of the custom input element to its saved state.
+	 * It also restores the form value and validity state in the form internals to their saved states.
+	 * @param {object} state The saved state of the custom input element.
+	 * @function
+	 */
+	formStateRestoreCallback(state) {
+		// Set the value of the custom input element to its saved value
+		this.value = state.value;
+		// Restore the form value in the form internals to the saved value
+		this.internals.setFormValue(state.value);
+		// Restore the validity state in the form internals to the saved state
+		this.internals.setValidity({});
+	}
 
-    /**
-     * The formStateSaveCallback method is a built-in lifecycle callback that gets called when the state of a form-associated custom element is saved.
-     * This method is responsible for saving the value of the custom input element.
-     * @returns {object} The saved state of the custom input element.
-     * @function
-     */
-    formStateSaveCallback() {
-        return {
-            value: this.value,
-        };
-    }
+	/**
+	 * The formStateSaveCallback method is a built-in lifecycle callback that gets called when the state of a form-associated custom element is saved.
+	 * This method is responsible for saving the value of the custom input element.
+	 * @returns {object} The saved state of the custom input element.
+	 * @function
+	 */
+	formStateSaveCallback() {
+		return {
+			value: this.value,
+		};
+	}
 
-    /**
-     * The formDisabledCallback method is a built-in lifecycle callback that gets called when the disabled state of a form-associated custom element changes.
-     * This method is not implemented yet.
-     * @param {boolean} disabled The new disabled state of the custom input element.
-     * @function
-     */
-    formDisabledCallback(disabled) {
-        console.warn('formDisabledCallback not implemented yet');
-    }
+	/**
+	 * The formDisabledCallback method is a built-in lifecycle callback that gets called when the disabled state of a form-associated custom element changes.
+	 * This method is not implemented yet.
+	 * @param {boolean} disabled The new disabled state of the custom input element.
+	 * @function
+	 */
+	formDisabledCallback(disabled) {
+		console.warn('formDisabledCallback not implemented yet');
+	}
 }
