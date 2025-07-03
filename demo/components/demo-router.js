47d6cf18a88409a08880223945bc7b633ed81f21
lencys <lukas.ondrejcek@gmail.com>
2025-07-03 09:04:50 +0200

    feat(router): add demo-router component and nested routes
    Introduced `demo-router.js` component to demonstrate routing functionalities with nested routes. Updated index file to include new routes and menu item for navigation.

---
+++ demo/components/demo-router.js
@@ -0,0 +1,59 @@
+import WJElement from '../../dist/wje-element.js';
+import CodeSnippet from '../assets/js/code-snippet-builder.js';
+
+const template = document.createElement('template');
+
+template.innerHTML = `<style>
+    .content {
+      display: block;
+    }
+  </style>
+  <h1>Status</h1>
+  <div class="container">
+
+    <!-- BASIC -->
+
+    <h2>Basic</h2>
+    <div class="playground">
+      <div class="content" style="display: flex; justify-content: center;">
+        <div style="width: 300px; height: 200px; position: relative; overflow: scroll">
+          <wje-router-outlet id="my-outlet-1" animation="fade"></wje-router-outlet>
+        </div>
+        <wje-menu id="navigation" variant="nav" active>
+            <wje-menu-item route="example-route-1">Route 1</wje-menu-item>
+            <wje-menu-item route="example-route-2">Route 2</wje-menu-item>
+        </wje-menu>
+        
+      </div>
+    </div>
+    </div>
+
+  </div>`;
+
+export default class DemoRouter extends WJElement {
+  constructor() {
+    super();
+  }
+
+  /**
+   * Retrieves the selector for the tab group outlet.
+   * @returns {string} The CSS selector for the tab group outlet element.
+   */
+  static get outlet() {
+    return '#my-outlet-1';
+  }
+
+  /**
+   * Returns the template for the component.
+   * @static
+   * @returns {HTMLElement} The template element
+   */
+  static get customTemplate() {
+    return template;
+  }
+}
+
+let __esModule = 'true';
+export { __esModule };
+
+customElements.get('demo-router') || window.customElements.define('demo-router', DemoRouter);
